39269bca3474db660d22d56d2dfc2fdfad2f0478
Ayano Hisamoto <ayano@AyanonoMacBook-Pro.local>
2024-01-01 23:48:50 +0900

    * Add date_validator gem * Fix date formatting in index.

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,10 +2,10 @@ class Schedule < ApplicationRecord
   
   validates :title, presence: true, length: { maximum: 20 }
   validates :start_time, presence: true
-  # validates :end_time, presence: true, date: { after_or_equal_to: :start_time }
-  validates :end_time, presence: true
+  validates :end_time, presence: true, date: { after_or_equal_to: :start_time }
   :all_day
   validates :memo, length: { maximum: 500 }
+  validate :formatted_start_time, :formatted_end_time
 
   def formatted_start_time
     start_time.strftime('%Y/%m/%d')
@@ -15,7 +15,4 @@ class Schedule < ApplicationRecord
     end_time.strftime('%Y/%m/%d')
   end
 
-  def formatted_updated_at
-    updated_at.in_time_zone('Tokyo').strftime('%Y/%m/%d %H:%M')
-  end
 end
